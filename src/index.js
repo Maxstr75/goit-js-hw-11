0da95a13f6e1140e4a9f79e10c646a37fb41fc1f
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-05-27 22:54:22 +0300

    add import notiflix

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@
 import SimpleLightbox from 'simplelightbox';
 // Дополнительный импорт стилей
 import 'simplelightbox/dist/simple-lightbox.min.css';
+// Дополнительный импорт notiflix
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
